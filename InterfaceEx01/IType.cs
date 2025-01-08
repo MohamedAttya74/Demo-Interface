11a4015fdba8d71e9623860a6bf74eb992e89c3d
ATTYA <ATTYA@DESKTOP-S6J1JOP>
2025-01-08 17:39:51 +0200

    Add project files.

---
+++ InterfaceEx01/IType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Interface.InterfaceEx01
+{
+    internal interface IType
+    {
+        // public :  access modifired  is the  
+
+
+
+    }
+}
